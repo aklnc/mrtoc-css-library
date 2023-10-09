64b876a332a5005292f72849437270876db3d53e
Aytaç Kılınç <47327815+aklnc@users.noreply.github.com>
2023-10-09 16:27:43 +0300

    Library Updated
    Colors and fonts are integrated.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './tryer.scss'
+
+function App() {
+  return (
+    <div className="app color-turquoise bgc-grey color-turquoiseHover">
+      <h1>This is a header</h1>
+    </div>
+  );
+}
+
+export default App;
